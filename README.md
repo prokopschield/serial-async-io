0d6859a7e2920d9c6ca6571d91987eec95008cd8
Prokop <prokop.schield@protonmail.com>
2021-07-04 17:31:32 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# serial-async-io
+
+read() and write() files via fs/promises, but one at a time.
+
+Install `yarn add serial-async-io`
+
+Import `import { read, write } from 'serial-async-io'`
+
+Or `import io from 'serial-async-io'`
+
+Both return promises.
---
+++ src/index.ts
@@ -0,0 +1,93 @@
+import fs from 'fs';
+import path from 'path';
+
+let wake = () => void trigger();
+
+const state = {
+	toRead: new Set<string>(),
+	toWrite: new Set<[string, () => void, Buffer | string]>(),
+	readPromises: new Map<string, Promise<Buffer>>(),
+	readCallbacks: new Map<string, (b: Buffer) => void>(),
+};
+
+/**
+ * Asynchronously read a file
+ * @param file_path Path to file, resolved to absolute path upon call
+ * @returns Promise of read buffer
+ */
+export function read(file_path: string): Promise<Buffer> {
+	file_path = path.resolve(file_path);
+	if (state.readPromises.has(file_path)) {
+		return state.readPromises.get(file_path) || read(file_path);
+	}
+	const promise = new Promise<Buffer>((resolve) => {
+		state.readCallbacks.set(file_path, (b: Buffer) => resolve(b));
+		state.toRead.add(file_path);
+		wake();
+	});
+	state.readPromises.set(file_path, promise);
+	return promise;
+}
+
+type TypedArray =
+	| Int8Array
+	| Uint8Array
+	| Uint8ClampedArray
+	| Int16Array
+	| Uint16Array
+	| Int32Array
+	| Uint32Array
+	| Float32Array
+	| Float64Array
+	| BigInt64Array
+	| BigUint64Array
+	| ArrayBuffer;
+
+/**
+ * Asynchronously write to a file
+ * @param file_path Path to file, resolved to absolute path upon call
+ * @param data Data `string` or `Buffer`
+ * @returns A promise that resolves to undefined
+ */
+export function write(
+	file_path: string,
+	data: TypedArray | string
+): Promise<void> {
+	file_path = path.resolve(file_path);
+	return new Promise((resolve) => {
+		// Don't keep a referrence to the data object
+		state.toWrite.add([
+			file_path,
+			resolve,
+			typeof data === 'string' ? data : Buffer.from(data),
+		]);
+		wake();
+	});
+}
+
+async function trigger(): Promise<void> {
+	wake = () => void null;
+	try {
+		for (const to_write of state.toWrite.values()) {
+			const [file, cb, buf] = to_write;
+			await fs.promises.writeFile(file, buf);
+			cb();
+			state.toWrite.delete(to_write);
+		}
+		for (const to_read of state.toRead.values()) {
+			const data = await fs.promises.readFile(to_read);
+			state.readCallbacks.get(to_read)?.(data);
+			state.readCallbacks.delete(to_read);
+			state.readPromises.delete(to_read);
+			state.toRead.delete(to_read);
+		}
+	} catch (error) {
+		wake = () => void trigger();
+		return void setTimeout(() => wake());
+	}
+	if (state.toRead.size || state.toWrite.size) {
+		setTimeout(() => trigger());
+	} else {
+		wake = () => void trigger();
+	}
+}
